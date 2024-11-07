5ead313030883f20f9a9b06969afa6e19880e746
Adeola Boye <adeoladev@hotmail.com>
2024-11-07 13:58:53 +0000

    Fixed bugs, improved syntax, removed unix and seconds format, added option to show date in short or long form.

--- easydate.js
+++ easydate.js
@@ -1,55 +1,40 @@
-function EDate(param) {
-
-    const self = {
-
-       format: (theformat) => {
+function EDate(param,theformat,type) {
 
         filter = param.split(' ');
         filter2 = parseInt(filter[0]);
         date = new Date();
         format = /[^dmy]/;
         myArr = theformat.split("");
-        seconds = "Error. Incorrect Format.";
 
-        if (!filter2 || theformat == null || (format.test(theformat) == true && theformat !== "unix" && theformat !== "seconds") 
-        || (myArr.length > 3 && theformat !== "unix" && theformat !== "seconds")) {
+        if ((!filter2 && param !== 'today') || theformat == null || type == null || format.test(theformat) == true || myArr.length > 3) {
           return "Error. Incorrect Format.";
         }
 
-        if (param.includes("days ago")) {
+        if (param == "today") {
+          date.setDate(date.getDate());
+          
+        } else if (param.includes("days ago") || param.includes("day ago")) {
           date.setDate(date.getDate()-filter2);
 
-        } else if (param.includes("weeks ago")) {
+        } else if (param.includes("weeks ago") || param.includes("week ago")) {
           filter3 = 7 * filter2;
           date.setDate(date.getDate()-filter3);
 
-        } else if (param.includes("months ago")) {
+        } else if (param.includes("months ago") || param.includes("month ago")) {
           filter3 = 30 * filter2;
           date.setDate(date.getDate()-filter3);
 
-        } else if (param.includes("days from now")) {
-          seconds = filter2 * 86400;
+        } else if (param.includes("days from now") || param.includes("day from now")) {
           date.setDate(date.getDate()+filter2);
 
-        } else if (param.includes("weeks from now")) {
+        } else if (param.includes("weeks from now") || param.includes("week from now")) {
           filter3 = 7 * filter2;
-          seconds = filter2 * 604800;
           date.setDate(date.getDate()+filter3);
 
-        } else if (param.includes("months from now")) {
+        } else if (param.includes("months from now") || param.includes("month from now")) {
           filter3 = 30 * filter2;
-          seconds = filter2 * 18144000;
           date.setDate(date.getDate()+filter3);
         }
-
-        if (theformat == "seconds") {
-          param = seconds;
-          return param;
-        } else if (theformat == "unix") {
-          unix = date.getTime()/1000;
-          param = Math.trunc(unix);
-          return param;
-        } else {
 
           for (let i = 0; i < myArr.length; i++)  {
             if (myArr[i] == "d")  {
@@ -63,12 +48,27 @@ function EDate(param) {
 
          param = myArr[0] + "/" + myArr[1] + "/" + myArr[2];
 
-        }
+         if (type && type == 'long') {
+          const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+          Arr2 = theformat.split("");
+          var monthKey = Arr2.indexOf("m");
+          var dayKey = Arr2.indexOf("d");
+          var yearKey = Arr2.indexOf("y");
+          var actualMonth = myArr[monthKey];
+          var actualDay = myArr[dayKey];
+          var actualYear = myArr[yearKey];
+          var monthString = months[actualMonth-1];
+
+          const longting = [];
+          longting[monthKey] = monthString;
+          longting[dayKey] = actualDay;
+          longting[yearKey] = actualYear;
+
+          param = longting[0]+" "+longting[1]+" "+longting[2];
+         }
+
+        
         
        return param;  
-       },
-    }
-
-    return self
 
 }
--- easydate.php
+++ easydate.php
@@ -1,5 +1,5 @@
 <?php
-function EDate($date,$format) {
+function EDate($date,$format,$type) {
 
     $array = explode(" ", $date);
     $format2 = str_replace( 'y', 'Y', $format );
@@ -8,14 +8,15 @@ function EDate($date,$format) {
     $m = $array2[1];
     $y = $array2[2];
     $num = $array[0];
-    $seconds = "Error. Incorrect Format";
 
-    if (is_numeric($num) == false || $format == null || (preg_match('/[^dmy]/', $format) && $format !== "unix" && $format !== "seconds")
-    || (count($array2) > 3 && $format !== "unix" && $format !== "seconds")) {
+    if ((is_numeric($num) == false && $date !== 'today') || $format == null || preg_match('/[^dmy]/', $format) || count($array2) > 3) {
         return "Error. Incorrect Format";
     }
     
-    if (strpos($date, "day ago") !== false || strpos($date, "days ago") !== false) {
+    if ($date == 'today') {
+    $unix = strtotime("now");
+
+    } else if (strpos($date, "day ago") !== false || strpos($date, "days ago") !== false) {
     $unix = strtotime("-$num days");
 
     } else if (strpos($date, "week ago") !== false || strpos($date, "weeks ago") !== false) {
@@ -26,23 +27,35 @@ function EDate($date,$format) {
 
     } else if (strpos($date, "day from now") !== false || strpos($date, "days from now") !== false) {
     $unix = strtotime("+$num days");
-    $seconds = $num * 86400;
 
     } else if (strpos($date, "week from now") !== false || strpos($date, "weeks from now") !== false) {
     $unix = strtotime("+$num weeks");
-    $seconds = $num * 604800;
 
     } else if (strpos($date, "month from now") !== false || strpos($date, "months from now") !== false) {
     $unix = strtotime("+$num months");
-    $seconds = $num * 18144000;
     }
 
-    if ($format == "unix") {
-    return $unix;
-    } else if ($format == "seconds") {
-    return $seconds;
-    } else {
-    return date($d."/".$m."/".$y,$unix);
+    $date = date($d."/".$m."/".$y,$unix);
+    $dateArray = explode("/",$date);
+
+    if ($type && $type == "long") {
+    $months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    $monthKey = array_search('m',$array2);
+    $dayKey = array_search('d',$array2);
+    $yearKey = array_search('Y',$array2);
+    $actualMonth = $dateArray[$monthKey];
+    $actualDay = $dateArray[$dayKey];
+    $actualYear = $dateArray[$yearKey];
+    $monthString = $months[$actualMonth-1];
+    $longting = [];
+
+    $longting[$monthKey] = $monthString;
+    $longting[$dayKey] = $actualDay;
+    $longting[$yearKey] = $actualYear;
+
+    return $longting[0]." ".$longting[1]." ".$longting[2];
     }
+
+    return $date;
 
 }
